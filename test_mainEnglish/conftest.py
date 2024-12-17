cd97cc6ab53f2fa414e2c6d666e70ffeca30b496
anor23 <f@>
2024-12-17 15:29:47 +0300

    написана новая фикстура для второго класса учитель дисциплины

--- test_mainEnglish/conftest.py
+++ test_mainEnglish/conftest.py
@@ -1,9 +1,14 @@
 import pytest
 
-from mainEnglish import Teacher
+from mainEnglish import Teacher, DisciplineTeacher
 
 
 @pytest.fixture
 def test_teacher():
     teacher = Teacher( "Антон","Алексеевич","Математика","3 года")
     return teacher
+
+@pytest.fixture
+def test_dicp_teacher():
+    disp_teach = DisciplineTeacher("Антон","Алексеевич","Математика","3 года","математика","завуч")
+    return disp_teach
