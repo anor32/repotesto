94a23855b5608def3eb1b8d3dfc5283047d9dfa4
anor23 <f@>
2024-12-17 14:36:00 +0300

    добавлены две новые функции нанять учителя и уволить добавлен список нанятых учителей

--- mainEnglish.py
+++ mainEnglish.py
@@ -1,4 +1,5 @@
 class Teacher:
+    teaches  = []
     def __init__(self, name, last_name, education, exp):
         self.name = name
         self.__last_name = last_name
@@ -59,23 +60,44 @@ class DisciplineTeacher(Teacher):
     def give_a_consultation(self, classwork):
         return f"{super().give_a_consultation(classwork)} Предмет {self.discipline}, Как {self.job_title}"
 
-
-
+    def hire_teacher(self,nameTeacher,lastNameTeacher):
+        if (nameTeacher,lastNameTeacher) not in Teacher.teaches:
+            Teacher.teaches.append((nameTeacher,lastNameTeacher))
+            return f"{nameTeacher} {lastNameTeacher} учитель успшено добавлен"
+        else:
+            return "не может быть добавлен такой учитель уже есть"
+    def fire_teacher(self,nameTeacher,lastNameTeacher):
+        if (nameTeacher,lastNameTeacher)  in Teacher.teaches:
+            Teacher.teaches.remove((nameTeacher,lastNameTeacher))
+            return f"{nameTeacher} {lastNameTeacher} успешно удален "
+        else:
+            return f"{nameTeacher} {lastNameTeacher} не найден в списке не может быть удален "
 # вы мне впрошлый раз написали чтобы  я улучшил не переписывал методы полностью
 # а использовал super()
 # я исправил  теперь я использую суперкласс внутри методов
 # я надеюсь я верно понял и именно это иммелось ввиду жду комментарий про этот момент
 
-# Создание объекта Teacher и отладка
-programer = Teacher("Дмитрий", "Сульжиц", 'МГУ', "10 лет")
-print(programer.get_teacher_data())
-print(programer.add_mark("Андрей Никанов", "12"))
-print(programer.remove_mark("Иван Иванов", "10"))
-print(programer.give_a_consultation("python426"))
 
-# Создание объекта DisciplineTeacher
-chemist = DisciplineTeacher("Волтер", "Вайт", "Высшее", "3", "Химия", "учитель")
-print(chemist.get_teacher_data())
-print(chemist.add_mark("джесси", "5"))
-print(chemist.remove_mark("джесси", "5"))
-print(chemist.give_a_consultation("сhemistry782"))
+
+# вызовы коментирую чтобы не портить покрытие тестов
+
+# # Создание объекта Teacher и отладка
+# programer = Teacher("Дмитрий", "Сульжиц", 'МГУ', "10 лет")
+# print(programer.get_teacher_data())
+# print(programer.add_mark("Андрей Никанов", "12"))
+# print(programer.remove_mark("Иван Иванов", "10"))
+# print(programer.give_a_consultation("python426"))
+#
+#
+# # Создание объекта DisciplineTeacher
+# chemist = DisciplineTeacher("Волтер", "Вайт", "Высшее", "3", "Химия", "учитель")
+# print(chemist.get_teacher_data())
+# print(chemist.add_mark("джесси", "5"))
+# print(chemist.remove_mark("джесси", "5"))
+# print(chemist.give_a_consultation("сhemistry782"))
+#
+# # новые функции
+#
+# print(chemist.hire_teacher("иван","долгович"))
+#
+# print(chemist.fire_teacher("иван","долгович"))
